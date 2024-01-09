7c9614da67f76d425a72e5bfd950c07ddbaeaeea
TaeHyoun Kim <kth9903@gmail.com>
2024-01-09 20:58:45 +0900

    refactor api controller and model to include json serialization and deserialization tests using objectmapper

--- src/main/java/com/example/restapi/model/UserRequest.java
+++ src/main/java/com/example/restapi/model/UserRequest.java
@@ -1,23 +1,65 @@
 package com.example.restapi.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserRequest {
+    @JsonProperty("user_names")  // json data와 매핑하기 위함 user_name이 아닌 user_names여서 매핑이 기본적으로 안되서 null리턴
     private String userName;
+
     private Integer userAge;
+
     private String email;
+
     private Boolean isKorean;  // setIsKorean(Boolean)이 생성됨 => "is_korean" : true 사용가능
 
-    //private boolean isKorean; //boolean타입 default 값이 false여서 setKorean()으로 만들어지게 됨
+    //    public String getName(){
+//        return this.userName;
+//    }
+//
+//    public int getHumanAge(){
+//        return this.userAge;
+//    }
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public Integer getUserAge() {
+//        return userAge;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public Boolean getIsKorean() {
+//        return isKorean;
+//    }
+//
+//    @JsonIgnore //object mapper 관여하는 클래스에서 json 으로 사용하지 않음을 명시
+//    public String getUser() {
+//        return userName;
+//    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "userName='" + userName + '\'' +
+                ", userAge=" + userAge +
+                ", email='" + email + '\'' +
+                ", isKorean=" + isKorean +
+                '}';
+    }
+//private boolean isKorean; //boolean타입 default 값이 false여서 setKorean()으로 만들어지게 됨
     //"is_korean" : true 으로 요청시 인식 못함 => "korean" : true 으로 바꾸면 요청가능하긴함(권장은 x)
     // primitive 타입이 아닌 reference 타입의 Boolean을 사용하는 것을 권장
 }
--- src/test/java/com/example/restapi/RestApiApplicationTests.java
+++ src/test/java/com/example/restapi/RestApiApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.restapi;
 
+import com.example.restapi.model.UserRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
 
+    @Autowired //spring에서 관리하는 bean들 중에 자동으로 생성된느 오브젝트 매퍼를 가저옴
+    private ObjectMapper objectMapper;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws JsonProcessingException {
+        //var user = new UserRequest("홍길동",10,"hong@gmail.com",true);
+        //System.out.println(user);
+//        user.setUserName("홍길동");
+//        user.setUserAge(10);
+//        user.setEmail("hong@gmail.com");
+//        user.setIsKorean(true);
+
+        //var json = objectMapper.writeValueAsString(user); //dto -> json (objectMapper를 통해 직렬화)
+        var json = "{\"user_names\":\"홍길동\",\"user_age\":10,\"email\":\"hong@gmail.com\",\"is_korean\":true}";
+        System.out.println(json);
+        //{"user_name":"홍길동","user_age":10,"email":"hong@gmail.com","is_korean":true}
+
+
+        var dto = objectMapper.readValue(json, UserRequest.class); //json -> dto (objectMapper를 통해 역직렬화)
+        System.out.println(dto);
+        //UserRequest(userName=홍길동, userAge=10, email=hong@gmail.com, isKorean=true)
     }
 
 }
