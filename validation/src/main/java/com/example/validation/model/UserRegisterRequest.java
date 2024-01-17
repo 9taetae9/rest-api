b5728f4334582f4a6c0ee9896dfa7b357990c2cf
TaeHyoun Kim <kth9903@gmail.com>
2024-01-17 22:11:11 +0900

    update userregisterrequest model to use @phonenumber for phonenumber field

--- validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
+++ validation/src/main/java/com/example/validation/model/UserRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.example.validation.model;
 
+import com.example.validation.annotation.PhoneNumber;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.*;
@@ -9,6 +10,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor // 기본 생성자
@@ -18,8 +20,10 @@ import java.time.LocalDateTime;
 public class UserRegisterRequest {
    //@NotNull  //!=nul
    //@NotEmpty //!=null && name !=""
-    @NotBlank //!=null && name !="" && name !=" "
+    //@NotBlank //!=null && name !="" && name !=" "
     private String name;
+
+    private String nickName;
 
     @Size(min=1, max =12) // 1~12
     @NotBlank
@@ -33,9 +37,25 @@ public class UserRegisterRequest {
     @Email
     private String email;
 
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.") //핸드폰 번호 정규식
+    //@Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 양식에 맞지 않습니다.") //핸드폰 번호 정규식
+    @PhoneNumber
     private String phoneNumber;
 
     @FutureOrPresent
     private LocalDateTime registerAt;
+
+    @AssertTrue(message =  "name or nickName은 존재해야 합니다.")
+    //@AssertFalse false를 리턴하여야함!
+    public boolean isNameCheck(){ //"is"를 붙여야 동작한다
+
+        if(Objects.nonNull(name) && !name.isBlank()){
+         return true;
+        }
+
+        if(Objects.nonNull(nickName) && !nickName.isBlank()){
+          return true;
+        }
+
+        return false; //둘다 null이거나 빈문자열일 경우 flase리턴
+    }
 }
