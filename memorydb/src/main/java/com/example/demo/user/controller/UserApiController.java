776b3ee17208ded1de056552cd39c34ce6538d0b
TaeHyoun Kim <kth9903@gmail.com>
2024-01-25 01:36:28 +0900

    enhance user module with score-based query methods and corresponding api endpoints

--- memorydb/src/main/java/com/example/demo/user/controller/UserApiController.java
+++ memorydb/src/main/java/com/example/demo/user/controller/UserApiController.java
@@ -53,4 +53,12 @@ public class UserApiController {
         return userService.filterScore(score);
     }
 
+    @GetMapping("/min_max")
+    public List<UserEntity> filterScore(
+            @RequestParam int min,
+            @RequestParam int max
+    ){
+        return userService.filterScore(min, max);
+    }
+
 }
--- memorydb/src/main/java/com/example/demo/user/db/UserRepository.java
+++ memorydb/src/main/java/com/example/demo/user/db/UserRepository.java
@@ -2,8 +2,29 @@ package com.example.demo.user.db;
 
 import com.example.demo.user.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+    //select * from user where score >= [??]
+    public List<UserEntity> findAllByScoreGreaterThanEqual(int score);
+
+    //select * from user where score >= ?? AND score <= ??
+    //쿼리 메소드 사용 권장
+    List<UserEntity> findAllByScoreGreaterThanEqualAndScoreLessThanEqual(int min, int max);
+
+    @Query( //jpql 방식으로 작성
+            //"select u from user u where u.score >= ?1 AND u.score <= ?2",
+            //native쿼리로 작성한 방식
+            value = "select * from user as u where u.score >= :min AND u.score <= :max",
+            nativeQuery = true
+    )
+    List<UserEntity> score(
+            @Param(value = "min") int min, //@param 으로 :min
+            @Param(value = "max") int max  //@param 으로 :max 로 각각 매칭 (named parameter)
+    );
 }
